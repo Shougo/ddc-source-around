b52e19f8d93d9b868842e58b211cebd5f7fe60b6
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-09-03 13:45:58 +0900

    Fix versions

--- denops/@ddc-sources/around.ts
+++ denops/@ddc-sources/around.ts
@@ -2,14 +2,14 @@ import {
   type DdcOptions,
   type Item,
   type SourceOptions,
-} from "jsr:@shougo/ddc-vim@7.0.0/types";
-import { BaseSource } from "jsr:@shougo/ddc-vim@7.0.0/source";
-import { convertKeywordPattern } from "jsr:@shougo/ddc-vim@7.0.0/utils";
+} from "jsr:@shougo/ddc-vim@~7.0.0/types";
+import { BaseSource } from "jsr:@shougo/ddc-vim@~7.0.0/source";
+import { convertKeywordPattern } from "jsr:@shougo/ddc-vim@~7.0.0/utils";
 
-import type { Denops } from "jsr:@denops/core@^7.0.0";
-import * as fn from "jsr:@denops/std@7.1.1/function";
+import type { Denops } from "jsr:@denops/core@~7.0.0";
+import * as fn from "jsr:@denops/std@~7.1.1/function";
 
-import { assertEquals } from "jsr:@std/assert@1.0.3/equals";
+import { assertEquals } from "jsr:@std/assert@~1.0.3/equals";
 
 const COLUMNS_MAX = 200;
 
