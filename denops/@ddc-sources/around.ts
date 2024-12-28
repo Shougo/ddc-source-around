32d54dc188b50bcd64f7b410c04aa4ec567ae47f
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-12-28 14:19:48 +0900

    Add minLength param

--- denops/@ddc-sources/around.ts
+++ denops/@ddc-sources/around.ts
@@ -2,28 +2,33 @@ import {
   type DdcOptions,
   type Item,
   type SourceOptions,
-} from "jsr:@shougo/ddc-vim@~7.0.0/types";
-import { BaseSource } from "jsr:@shougo/ddc-vim@~7.0.0/source";
-import { convertKeywordPattern } from "jsr:@shougo/ddc-vim@~7.0.0/utils";
+} from "jsr:@shougo/ddc-vim@~9.1.0/types";
+import { BaseSource } from "jsr:@shougo/ddc-vim@~9.1.0/source";
+import { convertKeywordPattern } from "jsr:@shougo/ddc-vim@~9.1.0/utils";
 
 import type { Denops } from "jsr:@denops/core@~7.0.0";
-import * as fn from "jsr:@denops/std@~7.1.1/function";
+import * as fn from "jsr:@denops/std@~7.4.0/function";
 
 import { assertEquals } from "jsr:@std/assert@~1.0.3/equals";
 
 const COLUMNS_MAX = 200;
 
-function allWords(lines: string[], pattern: string): string[] {
+function allWords(
+  lines: string[],
+  pattern: string,
+  minLength: number,
+): string[] {
   const words = lines
     .filter((line) => line.length < COLUMNS_MAX)
     .flatMap((line) => [...line.matchAll(new RegExp(pattern, "gu"))])
-    .filter((match) => match[0].length > 0)
+    .filter((match) => match[0].length >= minLength)
     .map((match) => match[0]);
   return Array.from(new Set(words)); // Remove duplication
 }
 
 type Params = {
   maxSize: number;
+  minLength: number;
 };
 
 export class Source extends BaseSource<Params> {
@@ -52,6 +57,7 @@ export class Source extends BaseSource<Params> {
     const cs: Item[] = allWords(
       await fn.getline(args.denops, minLines, maxLines),
       keywordPattern,
+      p.minLength,
     ).map((word) => ({ word }));
     return cs;
   }
@@ -59,14 +65,15 @@ export class Source extends BaseSource<Params> {
   override params(): Params {
     return {
       maxSize: 200,
+      minLength: 2,
     };
   }
 }
 
 Deno.test("allWords", () => {
-  assertEquals(allWords([], "\\w*"), []);
-  assertEquals(allWords(["_w2er"], "[a-zA-Z0-9_]+"), ["_w2er"]);
-  assertEquals(allWords(["asdf _w2er", "223r wawer"], "[a-zA-Z0-9_]+"), [
+  assertEquals(allWords([], "\\w*", 1), []);
+  assertEquals(allWords(["_w2er"], "[a-zA-Z0-9_]+", 1), ["_w2er"]);
+  assertEquals(allWords(["asdf _w2er", "223r wawer"], "[a-zA-Z0-9_]+", 1), [
     "asdf",
     "_w2er",
     "223r",
